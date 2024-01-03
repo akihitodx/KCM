18ed918fd66976b5bdb2ba980fb25abe23c07823
nino <1742441549@qq.com>
2024-01-03 16:29:03 +0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/KCM.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/KCM.dir/main.cpp.o"
+  "KCM"
+  "KCM.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/KCM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
